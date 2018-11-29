6d108d2a86b834e00cffa5017b21ee2865ffd3c3
zhoujian <zhoujian@newtank.cn>
2018-11-29 15:25:00 +0800

    spring-boot-cloud

--- tybest-eureka/src/main/java/com/tybest/eureka/EurekaApplication.java
+++ tybest-eureka/src/main/java/com/tybest/eureka/EurekaApplication.java
@@ -2,12 +2,15 @@ package com.tybest.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
+ * 单体Eureka服务
  * @author tb
  * @date 2018/11/29 15:10
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
