bf71cb6b9c21ab6fb778ce46e0bb410ccf6238f8
zhoujian <zhoujian@newtank.cn>
2018-11-29 16:27:07 +0800

    spring-boot-cloud

---
+++ tybest-eureka-cluster/src/main/java/com/tybest/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.tybest.eureka.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author tb
+ * @date 2018/11/29 16:24
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
