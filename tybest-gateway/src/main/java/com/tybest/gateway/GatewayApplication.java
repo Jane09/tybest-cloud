2cb22b95b51d4d6fb0f6817cb0443d361a9151ae
zhoujian <zhoujian@newtank.cn>
2018-11-29 15:11:10 +0800

    spring-boot-cloud

---
+++ tybest-gateway/src/main/java/com/tybest/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.tybest.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tb
+ * @date 2018/11/29 15:09
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+}
