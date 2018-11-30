94b737660617e7735cd1b5013912024a19d38d36
zhoujian <zhoujian@newtank.cn>
2018-11-30 15:16:40 +0800

    spring-boot-cloud

--- tybest-config-client/src/main/java/com/tybest/config/ConfigClientApplication.java
+++ tybest-config-client/src/main/java/com/tybest/config/ConfigClientApplication.java
@@ -1,17 +1,21 @@
 package com.tybest.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author tb
  * @date 2018/11/30 14:21
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientApplication implements CommandLineRunner {
 
-
+//    @Value("${tybest.hello}")
+    private String hello;
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class);
@@ -19,6 +23,6 @@ public class ConfigClientApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        System.out.println(hello);
     }
 }
--- tybest-eureka/src/main/java/com/tybest/eureka/config/WebSecurityConfig.java
+++ tybest-eureka/src/main/java/com/tybest/eureka/config/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().ignoringAntMatchers("/eureka/**");
+        http.csrf().disable()/*.ignoringAntMatchers("/eureka/**")*/;
         super.configure(http);
     }
 }
