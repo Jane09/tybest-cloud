0cab30e35a1854652d204c367180ed2e088b3c69
zhoujian <zhoujian@newtank.cn>
2018-11-30 14:15:22 +0800

    spring-boot-cloud

--- tybest-config/src/main/java/com/tybest/config/ConfigApplication.java
+++ tybest-config/src/main/java/com/tybest/config/ConfigApplication.java
@@ -1,5 +1,7 @@
 package com.tybest.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -10,9 +12,18 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigApplication {
+public class ConfigApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class);
+    }
+
+    @Value("${tybest.hello}")
+    private String hello;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(hello);
     }
 }
