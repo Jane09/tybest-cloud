64d1ad4c2d417ba1ef1f9877f2bb41c3617e9ae5
zhoujian <zhoujian@newtank.cn>
2018-11-30 14:39:18 +0800

    spring-boot-cloud

--- tybest-config-client/src/main/java/com/tybest/config/ConfigClientApplication.java
+++ tybest-config-client/src/main/java/com/tybest/config/ConfigClientApplication.java
@@ -3,23 +3,22 @@ package com.tybest.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author tb
- * @date 2018/11/30 14:05
+ * @date 2018/11/30 14:21
  */
 @SpringBootApplication
-@EnableConfigServer
-@EnableEurekaClient
-public class ConfigApplication implements CommandLineRunner {
+public class ConfigClientApplication implements CommandLineRunner {
+
+
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class);
+        SpringApplication.run(ConfigClientApplication.class);
     }
 
     @Override
     public void run(String... args) throws Exception {
+
     }
 }
